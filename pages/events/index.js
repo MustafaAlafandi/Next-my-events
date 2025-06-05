c4f1a8a060d6e98b1638f25821c88792b103ca55
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-06-05 19:39:38 +0300

    add method to go to filtered events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,18 @@
 import { getAllEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 import EventsList from "@/components/events/EventsList";
 import EventsSearch from "../../components/events/events-search";
 export default function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+  function findEventsHandler(year, month) {
+    let fullPath = "/events/" + year;
+    if (month) fullPath += "/" + month;
+    router.push(fullPath);
+  }
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventsList items={events} />
     </>
   );
