c1b8b022f6b34ad10987995fcddec6ee3157ab50
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-06-11 02:29:06 +0300

    change the dummy-data with firebase data and make the main page get the data from firebase by client-side

--- dummy-data.js
+++ dummy-data.js
@@ -1,42 +1,21 @@
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "Programming for everyone",
-    description:
-      "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
-    location: "Somestreet 25, 12345 San Somewhereo",
-    date: "2024-05-12",
-    image: "images/coding-event.jpg",
-    isFeatured: false,
-  },
-  {
-    id: "e2",
-    title: "Networking for introverts",
-    description:
-      "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
-    location: "New Wall Street 5, 98765 New Work",
-    date: "2024-05-30",
-    image: "images/introvert-event.jpg",
-    isFeatured: true,
-  },
-  {
-    id: "e3",
-    title: "Networking for extroverts",
-    description:
-      "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
-    location: "My Street 12, 10115 Broke City",
-    date: "2025-04-10",
-    image: "images/extrovert-event.jpg",
-    isFeatured: true,
-  },
-];
-
-export function getFeaturedEvents() {
-  return DUMMY_EVENTS.filter((event) => event.isFeatured);
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://nextjs-course-database-90f35-default-rtdb.firebaseio.com/events.json"
+  );
+  // console.log("response", response);
+  const data = await response.json();
+  // console.log("data", data);
+  const transformedData = [];
+  for (const key in data) {
+    transformedData.push({ ...data[key], id: key });
+  }
+  // console.log("transformedData", transformedData);
+  return transformedData;
 }
 
-export function getAllEvents() {
-  return DUMMY_EVENTS;
+export async function getFeaturedEvents() {
+  const data = await getAllEvents();
+  return data.filter((event) => event.isFeatured);
 }
 
 export function getFilteredEvents(dateFilter) {
@@ -53,6 +32,7 @@ export function getFilteredEvents(dateFilter) {
   return filteredEvents;
 }
 
-export function getEventById(id) {
-  return DUMMY_EVENTS.find((event) => event.id === id);
+export async function getEventById(id) {
+  const data = await getAllEvents();
+  return data.find((event) => event.id === id);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import { useEffect, useState } from "react";
 import { getFeaturedEvents } from "../dummy-data";
 import EventsList from "../components/events/EventsList";
 export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-  return (
-    <div>
-      <EventsList items={featuredEvents} />
-    </div>
-  );
+  const [featuredEvents, setFeaturedEvents] = useState();
+  useEffect(() => {
+    async function GFE() {
+      const FE = await getFeaturedEvents();
+      setFeaturedEvents(FE);
+    }
+    GFE();
+  }, []);
+  if (!featuredEvents || featuredEvents?.length === 0) return <p>Loading...</p>;
+  return <div>{<EventsList items={featuredEvents} />}</div>;
 }
