4623a07e1c4ed19647359152a5b952c076d9df4e
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-06-11 02:40:55 +0300

    get the featured events by server-side using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
 import { getFeaturedEvents } from "../dummy-data";
 import EventsList from "../components/events/EventsList";
-export default function HomePage() {
-  const [featuredEvents, setFeaturedEvents] = useState();
-  useEffect(() => {
-    async function GFE() {
-      const FE = await getFeaturedEvents();
-      setFeaturedEvents(FE);
-    }
-    GFE();
-  }, []);
-  if (!featuredEvents || featuredEvents?.length === 0) return <p>Loading...</p>;
-  return <div>{<EventsList items={featuredEvents} />}</div>;
+export default function HomePage(props) {
+  return <div>{<EventsList items={props.featuredEvents} />}</div>;
+}
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      featuredEvents,
+    },
+  };
 }
