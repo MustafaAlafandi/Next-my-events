1f85d1c05a6c32f51013241d41bfe3dfce3730ac
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-06-11 07:28:51 +0300

    make event details page dynamic page using getStaticPropsand getStaticPaths

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -2,13 +2,10 @@ import EventContent from "@/components/event-detail/event-content";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventSummary from "@/components/event-detail/event-summary";
 import ErrorAlert from "@/components/ui/error-alert";
-import { getEventById } from "@/dummy-data";
+import { getAllEvents, getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
-export default function EventDetailPage() {
-  const router = useRouter();
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
-
+export default function EventDetailPage(props) {
+  const event = props.event;
   if (!event) {
     return (
       <ErrorAlert>
@@ -31,4 +28,23 @@ export default function EventDetailPage() {
       </EventContent>
     </>
   );
+}
+export async function getStaticProps(context) {
+  const { params } = context;
+  const event = await getEventById(params.eventId);
+  return {
+    props: {
+      event,
+    },
+    revalidate: 10,
+  };
+}
+export async function getStaticPaths() {
+  const paths = (await getAllEvents()).map((e) => ({
+    params: { eventId: e.id },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
 }
