d4047de28898c35259d668d36b13bc029a3cbc3e
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-06-17 22:53:51 +0300

    Adding error handling to newsletter api

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,20 +1,37 @@
 import { MongoClient } from "mongodb";
-import "dotenv/config"
+import "dotenv/config";
+const url = process.env.MongoDBURL;
+const dbName = "auth";
+async function connectDB() {
+  const client = new MongoClient(url);
+  await client.connect();
+  return client;
+}
+async function insertDocument(client, document) {
+  const db = client.db(dbName);
+  const collection = db.collection("emails");
+  await collection.insertOne(document);
+}
 export default async function handler(req, res) {
   const email = req.body.email;
   if (req.method === "POST") {
     if (!email || !email.includes("@")) {
       res.status(422).json({ message: "Invalid email address. " });
     }
-    const url = process.env.MongoDBURL;
-    const dbName = "auth";
-    const client = new MongoClient(url);
-    await client.connect();
-    const db = client.db(dbName);
-    const collection = db.collection("emails");
-    await collection.insertOne({ email });
+    let client;
+    try {
+      client = await connectDB();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed." });
+      return;
+    }
+    try {
+      await insertDocument(client, { email });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed." });
+    }
     res.status(201).json({ message: "Signed Up" });
-    client.close();
   }
 }
 // mongodb+srv://mustafaalafandi1194:vE3COWazG2tg2Hfn@cluster0.2tp8rjv.mongodb.net/
